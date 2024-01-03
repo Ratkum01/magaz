30fc7ff2fce35e4bcbb5c7a0d501a543e85fa801
Nuriden Muktar <ratkum.2001@gmail.com>
2024-01-03 22:40:43 +0600

    urls and namespace

--- sitemagaz/main/urls.py
+++ sitemagaz/main/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 
 from . import views
 
+app_name= 'main'
 urlpatterns = [
     path("", views.index, name="index"),
     path("about", views.about, name='about'),
