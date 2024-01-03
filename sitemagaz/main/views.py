4e06a60a90df5c93d8960792eae02bf0e4e3bb35
Nuriden Muktar <ratkum.2001@gmail.com>
2024-01-03 22:01:48 +0600

    created base.html

--- sitemagaz/main/views.py
+++ sitemagaz/main/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    context= {
+        'title':'Главная страница',
+        'content':'This is CONTENT',
+    }
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    context= {
+        'title':'О нас',
+        'content':'This is АБОУТ',
+    }
+    return render(request, 'main/about.html', context)
