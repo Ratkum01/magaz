0dab29a9f610b535bc6ef19b8de394855d1bd746
Nuriden Muktar <ratkum.2001@gmail.com>
2024-01-04 11:43:37 +0600

    create model and admin

--- sitemagaz/goods/models.py
+++ sitemagaz/goods/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Categories (models.Model):
+    name= models.CharField(max_length=50, unique=True, verbose_name="Название")
+    slug= models.SlugField(max_length=100, unique= True, blank=True , null= True , verbose_name='URL')
+
+    class Meta:
+        verbose_name= 'Категорю'
+        verbose_name_plural= 'Категории'
