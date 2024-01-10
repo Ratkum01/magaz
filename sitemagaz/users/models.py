99575112d12752e8b2d4ebe0b0bfb1b25bed1016
Nuriden Muktar <ratkum.2001@gmail.com>
2024-01-10 23:21:32 +0600

    add models, admin for users

--- sitemagaz/users/models.py
+++ sitemagaz/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class User(AbstractUser):
+    images= models.ImageField(upload_to='user_images', blank=True, null=True, verbose_name='Аватар')
+
+    def __str__(self):
+        return self.username
+    
+    class Meta:
+        verbose_name= 'Пользователь'
+        verbose_name_plural= 'Пользователи'
