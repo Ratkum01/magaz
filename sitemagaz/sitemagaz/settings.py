a72122955b74c1c0f50ef149c6dd86735f0d069e
Nuriden Muktar <ratkum.2001@gmail.com>
2024-01-10 18:27:19 +0600

    add postgres

--- sitemagaz/sitemagaz/settings.py
+++ sitemagaz/sitemagaz/settings.py
@@ -87,11 +87,16 @@ WSGI_APPLICATION = "sitemagaz.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'home',
+        'USER': 'home',
+        'PASSWORD': 'home',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
