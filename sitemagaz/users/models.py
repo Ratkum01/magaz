6dd69a5f87e94c10c252d6b9baee0f34922263fe
Nuriden Muktar <ratkum.2001@gmail.com>
2024-01-12 02:02:59 +0600

    add fileform

--- sitemagaz/users/models.py
+++ sitemagaz/users/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
+
 # Create your models here.
 class User(AbstractUser):
-    images= models.ImageField(upload_to='user_images', blank=True, null=True, verbose_name='Аватар')
+    image = models.ImageField(
+        upload_to="users_images", blank=True, null=True, verbose_name="Аватар"
+    )
 
     def __str__(self):
         return self.username
-    
+
     class Meta:
-        verbose_name= 'Пользователь'
-        verbose_name_plural= 'Пользователи'
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
--- sitemagaz/users/views.py
+++ sitemagaz/users/views.py
@@ -2,11 +2,12 @@ from django.contrib import auth
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
-
-from users.forms import UserLoginForm, UserRegistrtionForm
-
+from django.contrib.auth.decorators import login_required
+from users.forms import ProfileForm, UserLoginForm, UserRegistrtionForm
+from django.contrib import auth, messages
 
 # Create your views here.
+
 def login(request):
     if request.method == "POST":
         form = UserLoginForm(data=request.POST)
@@ -16,6 +17,9 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request, f"{username} , Вы успешно зашли")
+                if request.POST.get(next, None):
+                    return HttpResponseRedirect(request.POST.get('next'))
                 return HttpResponseRedirect(reverse("main:index"))
     else:
         form = UserLoginForm()
@@ -28,8 +32,9 @@ def registration(request):
         form = UserRegistrtionForm(data=request.POST)
         if form.is_valid():
             form.save()
-            user= form.instance
+            user = form.instance
             auth.login(request, user)
+            messages.success(request, f"{user.username} , Вы успешно регнулись")
             return HttpResponseRedirect(reverse("main:index"))
     else:
         form = UserRegistrtionForm()
@@ -38,11 +43,22 @@ def registration(request):
     return render(request, "users/registration.html", context)
 
 
+@login_required
 def profile(request):
-    context = {"title": "Кабинет"}
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Профайл успешно обновлен")
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = ProfileForm(instance=request.user)
+
+    context = {"title": "Home - Кабинет", "form": form}
     return render(request, "users/profile.html", context)
 
-
+@login_required
 def logout(request):
+    messages.success(request, " Вы успешно вышли из аккаунта")
     auth.logout(request)
-    return redirect(reverse('main:index'))
+    return redirect(reverse("main:index"))
