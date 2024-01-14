485de4d4e5d8ebf246d4cb7ddad50db44d489f22
Nuriden Muktar <ratkum.2001@gmail.com>
2024-01-14 11:38:47 +0600

    created models Cart

--- sitemagaz/carts/models.py
+++ sitemagaz/carts/models.py
@@ -1,3 +1,47 @@
+from email.policy import default
 from django.db import models
+from goods.models import Product
+
+from users.models import User
+
+
+class CartQueryset(models.QuerySet):
+    def total_price(self):
+        return sum(cart.products_price() for cart in self)
+
+    def total_quantity(self):
+        if self:
+            return sum(cart.quntity for cart in self)
+        return 0
+
 
 # Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        verbose_name="Пользователь",
+        blank=True,
+        null=True,
+    )
+    product = models.ForeignKey(
+        to=Product, on_delete=models.CASCADE, verbose_name="Товар"
+    )
+    quantity = models.PositiveSmallIntegerField(default=0, verbose_name="Количество")
+    created_timestamp = models.DateTimeField(
+        auto_now_add=True, verbose_name="Дата добавление"
+    )
+    session_key = models.CharField(max_length=32, null=True, blank=True)
+
+
+class Meta:
+    verbose_name = "Корзина"
+    verbose_name_plural = "Корзина"
+
+
+def __str__(self):
+    return f"Корзина {self.user.username} | Товар {self.product.name} | Количество {self.quantity}"
+
+
+def product_prices(self):
+    return round(self.product.sell_price() * self.quantity, 2)
