6b410903a5f92eddc60185de71ad6c0016a0cd7f
Nuriden Muktar <ratkum.2001@gmail.com>
2024-01-15 17:40:04 +0600

    add cart remove

--- sitemagaz/carts/views.py
+++ sitemagaz/carts/views.py
@@ -16,11 +16,14 @@ def cart_add(request, product_slug):
                 cart.save()
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
-    return redirect(request.META['HTTP_REFERER'])
+    return redirect(request.META["HTTP_REFERER"])
+
 
 def cart_change(request, product_slug):
     ...
 
 
-def cart_remove(request, product_slug):
-    ...
+def cart_remove(request, product_id):
+    cart = Cart.objects.get(id=product_id)
+    cart.delete()
+    return redirect(request.META["HTTP_REFERER"])
