12929c83b4d19a47f78ac3995007401678eefb4d
Nuriden Muktar <ratkum.2001@gmail.com>
2024-01-17 15:49:25 +0600

    ispravlen dobavlenie

--- sitemagaz/carts/views.py
+++ sitemagaz/carts/views.py
@@ -9,7 +9,7 @@ from carts.utils import get_user_carts
 
 # Create your views here.
 def cart_add(request, ):
-    product_id: request.POST.get('product_id')
+    product_id = request.POST.get("product_id")
     product = Product.objects.get(id=product_id)
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product)
@@ -21,15 +21,16 @@ def cart_add(request, ):
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
     
-    user_cart=get_user_carts(request)
-    cart_items_html=render_to_string(
-        'carts/includes/included_cart.html', {'carts': user_cart}, request=request
-    )
-    responce_data={
-        'message':'Товар добавлен корзину',
-        'cart_items_html':cart_items_html
+    user_cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+
+    response_data = {
+        "message": "Товар добавлен в корзину",
+        "cart_items_html": cart_items_html,
     }
-    return JsonResponse(responce_data)
+
+    return JsonResponse(response_data)
 
 
 def cart_change(request, product_slug):
