73f9050f336d55f4e88c62d01c10c90431bf5d8e
Nuriden Muktar <ratkum.2001@gmail.com>
2024-01-21 13:50:55 +0600

    update admin orders

--- sitemagaz/orders/views.py
+++ sitemagaz/orders/views.py
@@ -7,9 +7,10 @@ from orders.forms import CreateOrderForm
 from orders.models import Order, OrderItem
 from django.contrib.auth.decorators import login_required
 
+
 @login_required
 def create_order(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CreateOrderForm(data=request.POST)
         if form.is_valid():
             try:
@@ -21,22 +22,23 @@ def create_order(request):
                         # Создать заказ
                         order = Order.objects.create(
                             user=user,
-                            phone_number=form.cleaned_data['phone_number'],
-                            requires_delivery=form.cleaned_data['requires_delivery'],
-                            delivery_address=form.cleaned_data['delivery_address'],
-                            payment_on_get=form.cleaned_data['payment_on_get'],
+                            phone_number=form.cleaned_data["phone_number"],
+                            requires_delivery=form.cleaned_data["requires_delivery"],
+                            delivery_address=form.cleaned_data["delivery_address"],
+                            payment_on_get=form.cleaned_data["payment_on_get"],
                         )
                         # Создать заказанные товары
                         for cart_item in cart_items:
-                            product=cart_item.product
-                            name=cart_item.product.name
-                            price=cart_item.product.sell_price()
-                            quantity=cart_item.quantity
-
+                            product = cart_item.product
+                            name = cart_item.product.name
+                            price = cart_item.product.sell_price()
+                            quantity = cart_item.quantity
 
                             if product.quantity < quantity:
-                                raise ValidationError(f'Недостаточное количество товара {name} на складе\
-                                                       В наличии - {product.quantity}')
+                                raise ValidationError(
+                                    f"Недостаточное количество товара {name} на складе\
+                                                       В наличии - {product.quantity}"
+                                )
 
                             OrderItem.objects.create(
                                 order=order,
@@ -51,22 +53,22 @@ def create_order(request):
                         # Очистить корзину пользователя после создания заказа
                         cart_items.delete()
 
-                        messages.success(request, 'Заказ оформлен!')
-                        return redirect('users:profile')
+                        messages.success(request, "Заказ оформлен!")
+                        return redirect("users:profile")
             except ValidationError as e:
                 messages.success(request, str(e))
-                return redirect('cart:order')
+                return redirect("cart:order")
     else:
         initial = {
-            'first_name': request.user.first_name,
-            'last_name': request.user.last_name,
-            }
+            "first_name": request.user.first_name,
+            "last_name": request.user.last_name,
+        }
 
         form = CreateOrderForm(initial=initial)
 
     context = {
-        'title': 'Home - Оформление заказа',
-        'form': form,
-        'orders': True,
+        "title": "Home - Оформление заказа",
+        "form": form,
+        "orders": True,
     }
-    return render(request, 'orders/create_order.html', context=context)
+    return render(request, "orders/create_order.html", context=context)
